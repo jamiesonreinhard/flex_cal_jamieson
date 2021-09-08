909c6a89fca26dfd5495a3a79866ee977abd56e6
Jamieson <jreinhard@tempore.com>
2021-09-08 10:02:28 -0500

    removed the footer and moved color picker and row inc/dec to a new popup modal

--- listeners.js
+++ listeners.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
+  
   $(document).on("input", "#colorpicker", function(){
     const backgroundColor = $(this).val();
     $("th, .rightcol").css("background-color", backgroundColor);
+  })
+
+  $(".gearIcon").click(function(){
+    $el = $(".settingsModal");
+    if ($el.is(":hidden")){
+      $el.css("display", "flex");
+    } else {
+      $el.css("display", "none");
+    }
+  })
+
+  $(".closeIcon").click(function(){
+    $(".settingsModal").css("display", "none");
   })
 });
